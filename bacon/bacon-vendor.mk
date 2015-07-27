85a7c323fce1c863cd82d5e6fb24321757ba1eb7
Joe Maples <frap129@gmail.com>
2015-07-26 21:36:46 -0400

    Update bacon-vendor.mk

--- bacon/bacon-vendor.mk
+++ bacon/bacon-vendor.mk
@@ -16,4 +16,4 @@
 
 # Pick up overlay for features that depend on non-open-source files
 
-$(call inherit-product, vendor/oneplus/bacon/bacon-vendor-blobs.mk)
+$(call inherit-product, vendor/oneplus-caf/bacon/bacon-vendor-blobs.mk)
